0a0675d2255040aa5febcf3241cc76f17f106c9c
DHRUVRastogi-123 <57324344+DHRUVRastogi-123@users.noreply.github.com>
2023-11-14 01:06:46 +0530

    Update AddNoteBar.jsx

--- frontend/src/Components/AddNoteBar/AddNoteBar.jsx
+++ frontend/src/Components/AddNoteBar/AddNoteBar.jsx
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './AddNoteBar.css';
 import { BiBellPlus } from "react-icons/bi";
 import { IoColorPaletteOutline } from "react-icons/io5";
+import {FaDropletSlash} from 'react-icons/fa6';
 import { MdDelete } from "react-icons/md";
 import { IoMdDoneAll } from "react-icons/io";
 import {AiOutlineClose} from "react-icons/ai"
@@ -10,14 +11,81 @@ import { CgNotes } from 'react-icons/cg';
 
 function AddNoteBar(props)
 {
-  function AddNote()
+  const fetchNotes = async () => {
+      try {
+          const response = await fetch("http://localhost:5000/get-notes", {
+          method: "GET",
+          credentials: "include",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          });
+          if (response.ok) {
+          const noteData = await response.json();
+          setNotes(noteData.notes);
+          if(noteData.notes.length>0){setNote(true);}
+          console.log(noteData.notes)
+          // Split tasks into active and completed
+          } else {
+          console.error("Failed to fetch tasks");
+          }
+      } catch (error) {
+          console.error("Error", error);
+      }
+      };
+  
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+  const [isColorClicked, setColorClicked] = useState(false);
+  const [color,setColor] = useState('white');
+
+  const handleColorClicked = () => {
+    if(isColorClicked)
+    {
+      setColorClicked(false);
+    }
+    else
+    {
+      setColorClicked(true);
+    }
+  }
+
+  const AddNote = async () => 
   {
+    setColor('white');
+    if(newNoteTitle === '')
+    {
+      console.log("Title is required.");
+    }
+    else
+    {try {
+      const response = await fetch("http://localhost:5000/note", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: newNoteTitle,
+          note: newNoteText,
+          color: color,
+          reminder: new Date(),
+        }),
+      });
+
+      if (response.ok) {
+        // const data = await response.json();
+        fetchNotes();
+        // alert(data.message);
+      } else {
+        console.error("Failed to add task");
+      }
+    } catch (error) {
+      console.error("Error", error);
+    }}
     setIsTextFocused(false);
-    setNotes([{
-      title: newNoteTitle,
-      text: newNoteText
-    },...notes])
-    console.log(notes)
     setNewNoteText('');
     setNewNoteTitle('');
     setNote(true);
@@ -33,7 +101,7 @@ function AddNoteBar(props)
 
   return(
     <div className="megadiv">
-      <div className="add-note-bar">
+      <div className="add-note-bar" style={{backgroundColor:color}}>
       <div className="add-note-bar-top">
       <textarea
             className="add-note-title"
@@ -67,8 +135,37 @@ function AddNoteBar(props)
         <li>
           <BiBellPlus />
         </li>
-        <li>
+        <li className='add-note-bar-icons-color'
+        onClick={handleColorClicked}>
           <IoColorPaletteOutline />
+          <div 
+          className='add-note-bar-icons-color-chooser'
+          style={{display:isColorClicked? 'flex':'none'}}>
+              <ul>
+                <li 
+                className='add-note-bar-icons-color-chooser-white'
+                onClick={()=>{setColor('white')}}
+                >
+                <FaDropletSlash />
+                </li>
+                <li 
+                className='add-note-bar-icons-color-chooser-red'
+                onClick={()=>{setColor('#faafa8')}}
+                ></li>
+                <li className='add-note-bar-icons-color-chooser-blue'
+                onClick={()=>{setColor('#aeccdc')}}
+                ></li>
+                <li className='add-note-bar-icons-color-chooser-cyan'
+                onClick={()=>{setColor('#efeff1')}}
+                ></li>
+                <li className='add-note-bar-icons-color-chooser-yellow'
+                onClick={()=>{setColor('#fff8b8')}}
+                ></li>
+                <li className='add-note-bar-icons-color-chooser-custom'
+                onClick={()=>{setColor('#b4ddd3')}}
+                ></li>
+              </ul>
+          </div>
         </li>
         <li>
           <MdDelete />
@@ -80,7 +177,7 @@ function AddNoteBar(props)
         </div>
       <div 
       className="add-note-bar-submit"
-      onClick={()=>AddNote()}
+      onClick={AddNote}
       >
         Add
       </div>
@@ -88,7 +185,7 @@ function AddNoteBar(props)
     </div>
     <div className="notes-display">
     {notes.map((note,index)=>{
-      return <Card key={index} title={note.title} note={note.text} />
+      return <Card key={index} id={note._id} title={note.title} note={note.note} color={note.color} />
     })}
     <div className="notes-initial-bg" style={{ display: isNote ? 'none' : 'flex' }}>
         <CgNotes size={80} color='rgba(0,0,0,.2)'/>
