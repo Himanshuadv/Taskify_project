b2448206acacb4fc383275f0c32a75244305b01e
Himanshu Tripathi <123529078+Himanshuadv@users.noreply.github.com>
2025-09-17 17:36:32 +0530

    Update baseUrl.js

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,4 +1,4 @@
 // https://taskify-project-app-ejdnfte7hthzevdj.eastasia-01.azurewebsites.net/
 //http://localhost:8000
-const baseurl= "https://taskify-project-app-ejdnfte7hthzevdj.eastasia-01.azurewebsites.net/"
+const baseurl= "https://taskify-project-app-ejdnfte7hthzevdj.eastasia-01.azurewebsites.net"
 export default baseurl
