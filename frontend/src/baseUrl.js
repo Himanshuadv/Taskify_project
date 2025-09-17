8a56545222e2fa018c7a54f195d45f5b1290eb0b
Himanshu Tripathi <123529078+Himanshuadv@users.noreply.github.com>
2025-09-18 01:16:23 +0530

    Update baseUrl.js

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,4 +1,5 @@
 // https://taskify-project-app-ejdnfte7hthzevdj.eastasia-01.azurewebsites.net/
 //http://localhost:8000
-const baseurl= "taskify-backend-bsatgjgdg2d8drg9.southeastasia-01.azurewebsites.net/api"
-export default baseurl
+const baseurl = "https://taskify-backend-bsatgjgdg2d8drg9.southeastasia-01.azurewebsites.net/api";
+export default baseurl;
+
